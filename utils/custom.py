828044c26c4dfe89c706edc68ab1ef18efd6e924
Shea <89158603+sheacon@users.noreply.github.com>
2023-01-02 18:15:19 -0600

    updated custom functions

--- utils/custom.py
+++ utils/custom.py
@@ -46,3 +46,12 @@ def f1_cv(model, X_train, y_train):
     print(score.round(decimals=4))
     print(score.mean().round(decimals=4))
     return score
+
+def cv_metrics(model, X_train, y_train):
+    accuracy_score = cross_val_score(model, X_train, y_train, scoring = 'accuracy').mean().round(3)
+    precision_score = cross_val_score(model, X_train, y_train, scoring = 'precision').mean().round(3)
+    recall_score = cross_val_score(model, X_train, y_train, scoring = 'recall').mean().round(3)
+    f1_score = cross_val_score(model, X_train, y_train, scoring = 'f1').mean().round(3)
+    scores = pd.Series(data = [accuracy_score, precision_score, recall_score, f1_score]
+                         , index = ['accuracy', 'precision', 'recall', 'f1'])
+    return scores
